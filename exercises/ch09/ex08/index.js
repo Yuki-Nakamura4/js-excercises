f5102f12ecfdb30439f0e7e9d77e8258da73fccd
Yuki-Nakamura4 <Yuki.Nakamura4@jp.ricoh.com>
2024-05-22 21:31:34 +0900

    ch09/ex08まで解答

---
+++ exercises/ch09/ex08/index.js
@@ -0,0 +1,70 @@
+// 目覚まし時計クラス
+export class AlarmClock {
+  constructor() {
+    this.state = "normal";
+  }
+
+  setState(state) {
+    this.state = state;
+  }
+
+  getState() {
+    return this.state;
+  }
+
+  setAlarm() {
+    switch (this.state) {
+      case "normal":
+        this.state = "alarmSet";
+        return "none";
+      default:
+        return "none";
+    }
+  }
+
+  cancelAlarm() {
+    switch (this.state) {
+      case "alarmSet":
+        this.state = "normal";
+        return "none";
+      case "alarmSounding":
+        this.state = "normal";
+        return "stopAlarm";
+      case "snoozing":
+        this.state = "normal";
+        return "none";
+      default:
+        return "none";
+    }
+  }
+
+  reachedToAlarmTime() {
+    switch (this.state) {
+      case "alarmSet":
+        this.state = "alarmSounding";
+        return "soundAlarm";
+      default:
+        return "none";
+    }
+  }
+
+  snooze() {
+    switch (this.state) {
+      case "alarmSounding":
+        this.state = "snoozing";
+        return "stopAlarm";
+      default:
+        return "none";
+    }
+  }
+
+  elapseSnoozeTime() {
+    switch (this.state) {
+      case "snoozing":
+        this.state = "alarmSounding";
+        return "soundAlarm";
+      default:
+        return "none";
+    }
+  }
+}
