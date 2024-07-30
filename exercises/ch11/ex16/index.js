765ea56c96c11f5fae7ba420d3c33036c4998fac
Yuki-Nakamura4 <Yuki.Nakamura4@jp.ricoh.com>
2024-07-30 16:46:49 +0900

    コメントを修正

--- exercises/ch11/ex16/index.js
+++ exercises/ch11/ex16/index.js
@@ -3,18 +3,16 @@ export function retryWithExponentialBackoff(func, maxRetry, callback) {
 
   const tryFunc = () => {
     if (func()) {
-      // funcがtrueを返した場合、成功としてcallbackを呼び出す
       callback(true);
     } else if (attempt < maxRetry) {
-      // funcがfalseを返し、かつ最大リトライ回数に達していない場合、指数関数的バックオフでリトライ
+      // 指数関数的バックオフでリトライ
       attempt++;
-      const waitTime = Math.pow(2, attempt - 1); // 待ち時間を計算
-      setTimeout(tryFunc, waitTime * 1000); // setTimeoutで非同期にリトライ
+      const waitTime = Math.pow(2, attempt - 1);
+      setTimeout(tryFunc, waitTime * 1000);
     } else {
-      // 最大リトライ回数に達した場合、失敗としてcallbackを呼び出す
       callback(false);
     }
   };
 
-  tryFunc(); // 関数の初回実行
+  tryFunc();
 }
