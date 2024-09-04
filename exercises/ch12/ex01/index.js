175b43343f88b624b7b3325cb0150b989abde63e
Yuki-Nakamura4 <Yuki.Nakamura4@jp.ricoh.com>
2024-09-05 02:29:22 +0900

    ch12/ex01修正

--- exercises/ch12/ex01/index.js
+++ exercises/ch12/ex01/index.js
@@ -1,19 +1,23 @@
 import { counterIter, counterGen } from "./iterable.js";
 
+console.log("======== iterator ========");
 // イテレータを作成
-const itr = counterIter(3);
+const itr = counterIter(3); // => counterIter
 
 // 明示的にメソッドを呼ぶと、valueとdoneプロパティを持つ反復結果オブジェクトが返る。
-console.log(itr.next()); // => { value: 1, done: false }
+console.log(itr.next()); // => CounterIter: next { value: 1, done: false }
 
 // 間接的にメソッドを呼ぶと、valueプロパティの値だけが返る。
 for (const c of itr) {
-  console.log(c); // => 2, 3
+  // => counterIter: Symbol.iterator => counterIter: next
+  console.log(c); // => 2
 }
+
+// イテレーションが終了すると、valueプロパティがundefinedで、doneプロパティがtrueの反復結果オブジェクトが返る。
 console.log(itr.next()); // => { value: undefined, done: true }
 
 // イテレーションを途中で終了させると、イテレータのreturn()メソッドが呼ばれる。
-console.log("======== call return() ========");
+console.log("==== call return ====");
 const itr2 = counterIter(3);
 for (const c of itr2) {
   console.log(c); // => 1
@@ -25,12 +29,16 @@ console.log("======== generator ========");
 const gen = counterGen(3); // => counterGen
 
 // ジェネレータ関数によって生成されたオブジェクトがイテレータインターフェースを満たしていることを確認する。
+// nextがあり、戻り値が反復結果オブジェクトであることを確認する。
+console.log("==== next ====");
 console.log(typeof gen.next === "function"); // => true
-console.log(typeof gen.return === "function"); // => true
-console.log(typeof gen.throw === "function"); // => true
-console.log(typeof gen[Symbol.iterator] === "function"); // => true
-
 console.log(gen.next()); // => counterGen: next { value: 1, done: false }
-console.log(gen.next()); // => counterGen: next { value: 2, done: false }
-console.log(gen.next()); // => counterGen: next { value: 3, done: false }
-console.log(gen.next()); // => counterGen: finally { value: undefined, done: true }
+
+// returnがあり、戻り値が反復結果オブジェクトであること、かつdoneプロパティがtrueであることを確認する。
+console.log("==== return ====");
+console.log(typeof gen.return === "function"); // => true
+console.log(gen.return()); // => counterGen: finally { value: undefined, done: true }
+
+// throwがあり、戻り値が反復結果オブジェクトであることを確認する。
+console.log("==== throw ====");
+console.log(typeof gen.throw === "function"); // => true
