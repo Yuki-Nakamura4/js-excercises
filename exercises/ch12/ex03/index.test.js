8918abe0815cd9e118b21cb8f9eb039f8c2a0949
Yuki-Nakamura4 <Yuki.Nakamura4@jp.ricoh.com>
2024-09-06 10:29:01 +0900

    ch12/ex03修正

--- exercises/ch12/ex03/index.test.js
+++ exercises/ch12/ex03/index.test.js
@@ -1,12 +1,14 @@
 import { counterGenerator } from ".";
 
 describe("counterGeneratorのテスト", () => {
-  it("throw()でリセットされること", () => {
+  it("throw()でリセットされ、その後またインクリメントできること", () => {
     const gen = counterGenerator();
     expect(gen.next().value).toBe(0);
     expect(gen.next().value).toBe(1);
     expect(gen.next().value).toBe(2);
     gen.throw();
     expect(gen.next().value).toBe(0);
+    expect(gen.next().value).toBe(1);
+    expect(gen.next().value).toBe(2);
   });
 });
