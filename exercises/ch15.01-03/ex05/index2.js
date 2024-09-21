26f4de397a51d7145863d68fa211e8a6f88b3dfa
Yuki-Nakamura4 <Yuki.Nakamura4@jp.ricoh.com>
2024-09-21 23:04:59 +0900

    お風呂入ってくる

--- exercises/ch15.01-03/ex05/index2.js
+++ exercises/ch15.01-03/ex05/index2.js
@@ -1,1 +1,5 @@
-document.getElementById("1000").innerHTML = "Hello";
+/* eslint-disable no-undef */
+document.addEventListener("DOMContentLoaded", () => {
+  /* eslint-disable no-undef */
+  document.getElementById("1000").innerHTML = "Hello";
+});
