431f8d267ae8a25f9e4162b5c0c952d21a918ae5
Yuki-Nakamura4 <Yuki.Nakamura4@jp.ricoh.com>
2024-11-08 02:13:19 +0900

    コミット漏れをコミット

--- exercises/ch15.04-10/ex09/index.js
+++ exercises/ch15.04-10/ex09/index.js
@@ -26,27 +26,52 @@ document.getElementById("image").addEventListener("change", (event) => {
 
     const imageData = originalCtx.getImageData(0, 0, img.width, img.height);
     const data = imageData.data;
+    const width = img.width;
+    const height = img.height;
 
-    // グレースケールへの変換 (RGB を足して平均を取っている)
-    //
-    // ガウシアンフィルタを実装する場合はこの周辺のコードを変更しなさい
-    // imageData の中身はそのままに別の配列に結果を格納するとよい
-    // ```js
-    // const outputData = new Uint8ClampedArray(imageData.data.length);
-    //
-    // // TODO: ここで imageData.data を参照して outputData に結果を格納
-    //
-    // const outputImageData = new ImageData(outputData, img.width, img.height);
-    // filteredCtx.putImageData(outputImageData, 0, 0);
-    // ```
-    for (let i = 0; i < data.length; i += 4) {
-      const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
-      data[i] = avg;
-      data[i + 1] = avg;
-      data[i + 2] = avg;
+    // 出力用のデータ配列
+    const outputData = new Uint8ClampedArray(data.length);
+
+    // 5x5 のガウシアンカーネル（重み行列）
+    const kernel = [
+      [1, 4, 6, 4, 1],
+      [4, 16, 24, 16, 4],
+      [6, 24, 36, 24, 6],
+      [4, 16, 24, 16, 4],
+      [1, 4, 6, 4, 1],
+    ];
+    const kernelSum = 256; // カーネルの合計値で正規化
+
+    // ガウシアンぼかしの適用
+    for (let y = 2; y < height - 2; y++) {
+      for (let x = 2; x < width - 2; x++) {
+        let r = 0,
+          g = 0,
+          b = 0;
+
+        // カーネルを適用してぼかし処理
+        for (let ky = -2; ky <= 2; ky++) {
+          for (let kx = -2; kx <= 2; kx++) {
+            const pixelIndex = ((y + ky) * width + (x + kx)) * 4;
+            const weight = kernel[ky + 2][kx + 2];
+
+            r += data[pixelIndex] * weight;
+            g += data[pixelIndex + 1] * weight;
+            b += data[pixelIndex + 2] * weight;
+          }
+        }
+
+        const outputIndex = (y * width + x) * 4;
+        outputData[outputIndex] = r / kernelSum;
+        outputData[outputIndex + 1] = g / kernelSum;
+        outputData[outputIndex + 2] = b / kernelSum;
+        outputData[outputIndex + 3] = data[outputIndex + 3]; // アルファ値はそのまま保持
+      }
     }
 
-    filteredCtx.putImageData(imageData, 0, 0);
+    // 処理結果をキャンバスに表示
+    const outputImageData = new ImageData(outputData, img.width, img.height);
+    filteredCtx.putImageData(outputImageData, 0, 0);
   });
 
   reader.readAsDataURL(file);
