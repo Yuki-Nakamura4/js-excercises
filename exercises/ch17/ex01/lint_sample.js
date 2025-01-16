2b66c0d362ab48db64e63da98d484f2a95569bf8
Yuki-Nakamura4 <Yuki.Nakamura4@jp.ricoh.com>
2025-01-16 12:42:26 +0900

    ESLintを適用

--- exercises/ch17/ex01/lint_sample.js
+++ exercises/ch17/ex01/lint_sample.js
@@ -1,10 +1,8 @@
-let a, x, y;
 const r = 10;
 
-with (Math){
-  a = PI  * r * r;
-  x = r * cos(PI);
-  y = r * sin(PI / 2);
-}
+const PI = Math.PI;
+const a = PI  * r * r;
+const x = r * cos(PI);
+const y = r * sin(PI / 2);
 
 console.log(a, x, y);
